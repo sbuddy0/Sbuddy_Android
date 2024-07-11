dc2bb936d1fde051c7e9fbadfb0ef0b60bea11f9
pandabaeung <69721328+barti971219@users.noreply.github.com>
2024-07-12 03:09:16 +0900

    작성자 : 노희영 작성일 : 2024.07.12 설명 : 통신 확인

--- Sbuddy_Android/app/src/main/java/com/sbuddy/sbdApp/LoginActivity.kt
+++ Sbuddy_Android/app/src/main/java/com/sbuddy/sbdApp/LoginActivity.kt
@@ -2,10 +2,15 @@ package com.sbuddy.sbdApp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.sbuddy.sbdApp.http.RetrofitService
+import com.sbuddy.sbdApp.http.User
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,37 +27,62 @@ class LoginActivity : AppCompatActivity() {
 
         loginBtn = findViewById(R.id.login_btn)
 
+        val clientBuilder = OkHttpClient.Builder()
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+        clientBuilder.addInterceptor(loggingInterceptor)
+
         val retrofit = Retrofit.Builder()
-            .baseUrl("http://mellowcode.org/")
+            .baseUrl("http://13.238.152.155:8082/")
             .addConverterFactory(GsonConverterFactory.create())
+            .client((clientBuilder.build()))
             .build()
         val retrofitService = retrofit.create(RetrofitService::class.java)
 
-        val loginMap = HashMap<String, Any>()
-
+//        val loginMap = HashMap<String, String>()
+//        loginMap.put("id", "tmddus2123")
+//        loginMap.put("password", "1234")
 
         loginBtn.setOnClickListener{
 
-//            retrofitService.login(loginMap).enqueue(object: Callback<Map<String, Any>> {
-//                override fun onResponse(call: Call<Map<String, Any>>, response: Response<Map<String, Any>>) {
-//                    if(response.isSuccessful){
-//
-//                        Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
-//
-//                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
-//                        startActivity(intent);
-//                        finish()
-//
-//                    }
-//                }
-//
-//                override fun onFailure(call: Call<Map<String, Any>>, t: Throwable) {
-//                }
-//            })
 
-            val intent = Intent(this@LoginActivity, MainActivity::class.java)
-            startActivity(intent);
-            finish()
+            val user = User("tmddus2123", "1234")
+            retrofitService.login(user).enqueue(object: Callback<Object> {
+
+                override fun onResponse(call: Call<Object>, response: Response<Object>) {
+                    Log.d("sbuddyy", "response : ${response}")
+                    if(response.isSuccessful){
+
+                        Toast.makeText(this@LoginActivity, "로그인 성공", Toast.LENGTH_SHORT).show()
+
+                        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                        startActivity(intent);
+                        finish()
+
+                    }
+                }
+
+                override fun onFailure(call: Call<Object>, t: Throwable) {
+                    Log.d("sbuddyy", "로그인 실패 : " + t.message)
+                    Toast.makeText(this@LoginActivity, "로그인 실패 + ${t.message}", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+//            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+//            startActivity(intent);
+//            finish()
+//            val map = HashMap<String, String>()
+//            retrofitService.list(map).enqueue(object: Callback<Object>{
+//                override fun onResponse(call: Call<Object>, response: Response<Object>) {
+//                    Log.d("sbuddyy", "response : ${response}")
+//                }
+//
+//                override fun onFailure(call: Call<Object>, t: Throwable) {
+//                    Log.d("sbuddyy", "로그인 실패 : " + t.message)
+//                    TODO("Not yet implemented")
+//                }
+//
+//            })
 
         }
     }
--- Sbuddy_Android/app/src/main/java/com/sbuddy/sbdApp/http/RetrofitService.kt
+++ Sbuddy_Android/app/src/main/java/com/sbuddy/sbdApp/http/RetrofitService.kt
@@ -1,18 +1,33 @@
 package com.sbuddy.sbdApp.http
 
+import com.google.gson.annotations.SerializedName
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.FieldMap
 import retrofit2.http.FormUrlEncoded
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface RetrofitService {
 
-    @POST("user/login/")
-    @FormUrlEncoded
+    @POST("api/v1/login")
+    @Headers("Content-Type: application/json")
     fun login(
-        @FieldMap params: Map<String, Any>
-    ): Call<Map<String, Any>>
+        @Body params: User
+    ): Call<Object>
 
+
+    @POST("api/v1/post/popular/list")
+    @Headers("Content-Type: application/json")
+//    @FormUrlEncoded
+    fun list(
+        @FieldMap params: Map<String, String>
+    ): Call<Object>
 
 }
+
+data class User(
+    @SerializedName(value = "id") var id: String,
+    @SerializedName(value = "password") var password: String
+)
 
